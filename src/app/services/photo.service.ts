349f177d0744563202950b2ff1cef27206399b45
0nk3 <0nk3.0nk3@gmail.com>
2020-09-29 12:49:06 +0200

    gallery now runs on multiple platforms i.e browser, iphone and android

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Platform } from "@ionic/angular";
 import {
   Plugins,
   CameraResultType,
@@ -15,7 +16,11 @@ const { Camera, Filesystem, Storage } = Plugins;
 export class PhotoService {
   public photos: Photo[] = [];
   private PHOTO_STORAGE = "photos";
-  constructor() {}
+
+  private platform: Platform;
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
 
   public async addNewToGallery(): Promise<void> {
     const capturePhoto = await Camera.getPhoto({
@@ -33,15 +38,16 @@ export class PhotoService {
   public async loadSaved(): Promise<void> {
     const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photoList.value) || [];
-
-    for (const photo of this.photos) {
-      const readFile = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: FilesystemDirectory.Data,
-      });
+    if (!this.platform.is("hybrid")) {
+      for (const photo of this.photos) {
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
 
       // Web platform only: Load the photo as base64 data
-      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
     }
   }
   private async savePicture(cameraPhoto: CameraPhoto) {
@@ -52,15 +58,29 @@ export class PhotoService {
       data: base64Data,
       directory: FilesystemDirectory.Data,
     });
-    return {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath,
-    };
+    if (this.platform.is("hybrid")) {
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath,
+      };
+    }
   }
   async readAsBase64(cameraPhoto: CameraPhoto) {
-    const response = await fetch(cameraPhoto.webPath);
-    const blob = await response.blob();
-    return (await this.convertBlobToBase64(blob)) as string;
+    if (this.platform.is("hybrid")) {
+      const file = await Filesystem.readFile({
+        path: cameraPhoto.path,
+      });
+      return file.data;
+    } else {
+      const response = await fetch(cameraPhoto.webPath);
+      const blob = await response.blob();
+      return (await this.convertBlobToBase64(blob)) as string;
+    }
   }
   convertBlobToBase64 = (blob: Blob) =>
     // tslint:disable-next-line: no-shadowed-variable
