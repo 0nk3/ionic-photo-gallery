cce863a1ff334b39dd566ac212a5cfa0fc56f2b1
0nk3 <0nk3.0nk3@gmail.com>
2020-09-29 12:05:46 +0200

    saving the file in the filesystem done

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -7,6 +7,8 @@ import {
   CameraPhoto,
   CameraSource,
 } from "@capacitor/core";
+import { rejects } from "assert";
+import { resolve } from "dns";
 
 const { Camera, Filesystem, Storage } = Plugins;
 @Injectable({
@@ -22,11 +24,37 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100,
     });
-    this.photos.unshift({
-      filepath: "soon . . .",
-      webviewPath: capturePhoto.webPath,
-    });
+    const savedImageFile = await this.savePicture(capturePhoto);
+    this.photos.unshift(savedImageFile);
   }
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    const base64Data = await this.readAsBase64(cameraPhoto);
+    const fileName = new Date().getTime() + ".jpeg";
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data,
+    });
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath,
+    };
+  }
+  async readAsBase64(cameraPhoto: CameraPhoto) {
+    const response = await fetch(cameraPhoto.webPath);
+    const blob = await response.blob();
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+  convertBlobToBase64 = (blob: Blob) =>
+    // tslint:disable-next-line: no-shadowed-variable
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
 }
 export interface Photo {
   filepath: string;
